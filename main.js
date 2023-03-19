2c49873fc7e84a5ec13815665f133c043a6736e2
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-03-19 17:21:26 +0800

    Dynamic Version
    Features:
    1. Added javascript on the HTML
    2. Validation of bill and number of people
    3. Implemented Active state for inputs
    4. Dynamic Tip and Total amount
    
    WIP:
    1. Select Tip
    
    Problem Encounter for select tip on the radio buttons,
    All of the radio buttons can be selected.

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("hello world")
+const inputNumber = document.querySelectorAll('.input__tip');
+const invalidMessage = document.querySelectorAll('.invalid__message');
+const tipTotalAmount = document.querySelectorAll('.tip-total__amount');
+const radioBtnContainer = document.querySelectorAll('.select-tip__control');
+const radioBtn = document.querySelectorAll('.select-tip__radio');
+
+inputNumber.forEach((node, i) => {
+  node.addEventListener('blur', (e) => {
+    if (+e.target.value <= 0) {
+      invalidMessage[i].style.display = 'block';
+      node.style.border = '2px solid #ff8080';
+      tipTotalAmount[i].textContent = '$0.00';
+    } else {
+      invalidMessage[i].style.display = 'none';
+      node.style.border = 'none';
+      tipTotalAmount[i].textContent = `$${e.target.value}`;
+    }
+  });
+});
+
+radioBtn.forEach((node, i) => {
+  node.addEventListener('click', (e) => {
+    if (e.target.checked) {
+      radioBtnContainer[i].style.backgroundColor = '#ABEEE6';
+      radioBtnContainer[i].children[0].style.color = 'hsl(183, 100%, 15%)';
+    }
+  });
+});
