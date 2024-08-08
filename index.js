68da6a8cea4a7482c7db15c1561c26fd3bd6d1cc
ralphvirtucio <ralphvirtucio3@gmail.com>
2024-08-08 23:03:13 +0800

    Feature: Active and Empty State

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello world :)')
+const form = document.querySelector('.spl__form')
+
+
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  
+  console.log(data);
+  
+}
+
+
+form.addEventListener('change', handleSubmit)
